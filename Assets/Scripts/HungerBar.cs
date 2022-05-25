6d436afeedcbd16ff68f6f1804d78f79dbcfcdfc
David <davidas00oo@gmail.com>
2022-05-25 10:18:08 +0200

    UI Implemented

--- Assets/Scripts/HungerBar.cs
+++ Assets/Scripts/HungerBar.cs
@@ -3,23 +3,26 @@ using UnityEngine.UI;
 public class HungerBar : MonoBehaviour
 {
     public static HungerBar Instance;
-    public Slider slider;
+    public Image slider;
+    
     [SerializeField]
     private int speed=10;
     void Start()
     {
+        //slider = this.GetComponent<Image>();
+        //slider.fillAmount = 100f;
     }
 
     //100=1 so amount/100
     public void Add(float amount) { 
-        slider.value+= amount / 100;
+        slider.fillAmount += amount / 100;
     }
     public void Decrease(float amount)
     {
-        slider.value -= amount / 100;
+        slider.fillAmount -= amount / 100;
     }
     public int GetMoralAmount() { 
-        return (int)(slider.value*100);
+        return (int)(slider.fillAmount*100);
     }
     private void Update()
     {

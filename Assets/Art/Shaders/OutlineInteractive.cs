7f2f3bf3933fd251da05d075a9654b6ec9f9d057
Mark Kappen <466372@student.saxion.nl>
2022-05-31 10:48:38 +0200

    Added the shader graph for the outline

--- Assets/Art/Shaders/OutlineInteractive.cs
+++ Assets/Art/Shaders/OutlineInteractive.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class OutlineInteractive : MonoBehaviour
 {
-    Material outlineMaterial;
+    public Material outlineMaterial;
 
     // Start is called before the first frame update
     void Start()
+    {
+        CreateOutline();
+    }
+
+    private void CreateOutline()
     {
         GameObject outlineObject = new GameObject();
 
         outlineObject.AddComponent(typeof(MeshFilter));
         outlineObject.AddComponent(typeof(MeshRenderer));
-        outlineObject.GetComponent<MeshFilter>().mesh = this.GetComponent<MeshFilter>().mesh;
+        outlineObject.GetComponent<MeshFilter>().mesh = GetComponent<MeshFilter>().mesh;
 
-        outlineObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        outlineObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
 
-        outlineMaterial = GetComponent<Renderer>().material;
-        outlineMaterial.color = Color.red;
+        outlineObject.GetComponent<Renderer>().material = outlineMaterial;
 
-        outlineObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-
-        //Instantiate(outlineObject, outlineObject.transform.position, Quaternion.identity);
         Debug.Log("damn bro ");
     }
 

c4a6fcd452ebdd52a181ec9032dfda8bd391531c
DejaVu <whzbrady2@hotmail.com>
2022-06-04 19:38:36 +0200

    planty messy cleaning

--- Assets/Scripts/2dObjScripts/PlayerState/PlayerStateManager.cs
+++ Assets/Scripts/2dObjScripts/PlayerState/PlayerStateManager.cs
@@ -9,86 +9,61 @@ public class PlayerStateManager : MonoBehaviour
     //state
     PlayerBaseState currentState;
     PlayerState states;
-    public PlayerBaseState CurrentState {
-        get { return currentState; }
-        set { currentState = value; }
-    }
+
     [Header("Animation")]
     [SerializeField]
     private Animator animator;
-    public Animator Animator
-    {
-        get { return animator; }
-        set { animator = value; }
-    }
+   
     [Header("Movement")]
     private Rigidbody rb;
-    public Rigidbody RB { get { return rb; } set { rb = value; } }
-
     private float moveSpeed;
     [SerializeField]
     private float runSpeed = 8f;
     [SerializeField]
     private float walkSpeed = 4f;
-    public float MoveSpeed
-    {
-        get { return moveSpeed; }
-        set { moveSpeed = value; }
-    }
-
+   
     [SerializeField]
     private float interactRange = 1f;
-    public float GetInteractRange {
-        get { return interactRange; }
-    }
     private Vector3 moveDir;
-    public Vector3 MoveDir {
-        get { return moveDir; }
-        set { moveDir = value; }
-    }
-
+    
     [Header("Input")]
     InputPlayerControl Input;
-    public InputPlayerControl InPut
-    {
-        get { return Input; }
-
-    }
+    
     [Header("Interaction")]
     [SerializeField]
     private GameObject target = null;
     private bool switchedTarget = false;
     private GameObject OutlinedTarget;
     private GameObject Interacticon;
-    public GameObject InteractIcon { get { return Interacticon; } }
-    public GameObject Target {
-        get { return target; }
-        set { target = value; }
-    }
+    
+    
     [SerializeField]
     private Material outlineM;
+    private bool pushing = false;
+    int[] puzzleList;
+    private bool runing = false;
+
+    public PlayerBaseState CurrentState { get { return currentState; } set { currentState = value; } }
+    public Animator Animator { get { return animator; } set { animator = value; } }
+    public Rigidbody RB { get { return rb; } set { rb = value; } }
+    public float MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
+    public float GetInteractRange { get { return interactRange; } }
+    public Vector3 MoveDir { get { return moveDir; } set { moveDir = value; } }
+    public GameObject Target { get { return target; } set { target = value; } }
+    public InputPlayerControl InPut { get { return Input; } }
+    public GameObject InteractIcon { get { return Interacticon; } }
     public Material GetOutLineMaterial { get { return outlineM; } }
+    public bool Running { get { return runing; } set { runing = value; } }
+    public int[] PuzzleList { get { return puzzleList; } set { puzzleList = value; } }
+    public bool Pushing { get { return pushing; } set { pushing = value; } }
+
+ 
 
     [Header("UI Tutorials")]
     [SerializeField]
     bool tutorialIsOn = false;
     int tutSelector;
     public GameObject[] tutorials;
-
-    private bool pushing=false;
-    public bool Pushing {
-        get { return pushing; }
-        set { pushing = value; }
-    }
-    int currentInput;
-    int[] puzzleList;
-    public int[] PuzzleList
-    {
-        get { return puzzleList; }
-        set { puzzleList = value; }
-    }
-    private bool runing=false;
-    public bool Running { get { return runing; } set { runing = value; } }
     private void Awake()
     {
         //state
@@ -138,7 +113,6 @@ public class PlayerStateManager : MonoBehaviour
         currentState.UpdateState();
       //  UpdateRenderOrder();
         ShowOutLine();
-        //Debug.Log(currentState);
         //tutorialLogic();
     }
     
@@ -152,13 +126,6 @@ public class PlayerStateManager : MonoBehaviour
             CreatedoutlineObject.GetComponent<Renderer>().material = outlineM;
 
             CreatedoutlineObject.GetComponent<Renderer>().shadowCastingMode = ShadowCastingMode.Off;
-
-            //Maybe code from mark
-            //     if (CreatedoutlineObject.GetComponent<SortingGroup>() != null)
-            //     {
-            //         CreatedoutlineObject.GetComponent<SortingGroup>().sortingOrder = Target.GetComponentInChildren<SortingGroup>().sortingOrder - 1;
-            //     }
-     
 
             if (OutlinedTarget==null)
             {
@@ -183,22 +150,6 @@ public class PlayerStateManager : MonoBehaviour
         }
         
     }
-    //code from mark
-  //  private void UpdateRenderOrder()
-  //  {
-  //      if (Target != null && Target.GetComponentInChildren<SortingGroup>() != null)
-  //      {
-  //          if (transform.position.z < Target.transform.position.z)
-  //          {
-  //              Target.GetComponentInChildren<SortingGroup>().sortingOrder = 1;
-  //          }
-  //          else
-  //          {
-  //              Target.GetComponentInChildren<SortingGroup>().sortingOrder = 20;
-  //          }
-  //      }
-  //  }
-
     public void DestoryOutLinedTarget() {
         if (OutlinedTarget)
         {
